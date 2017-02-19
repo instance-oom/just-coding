2d4fc5736535a20b8e61fc83a994be0b2a757709
lon <dove2580@live.com>
2017-02-19 19:30:02 +0800

    add mergeSort

---
+++ sort/merge-sort.js
@@ -0,0 +1,56 @@
+let mergeSort = (arr, low, high) => {
+  if (low === null || low === undefined) low = 0;
+  if (high === null || high === undefined) high = arr.length - 1;
+  let mid = Math.floor((low + high) / 2);
+  if (low < high) {
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+  }
+  return arr;
+}
+
+let merge = (arr, low, mid, high) => {
+  let temp = [];
+  let i = low;
+  let j = mid + 1;
+  let k = 0;
+
+  while (i <= mid && j <= high) {
+    if (arr[i] <= arr[j]) {
+      temp[k] = arr[i];
+      i++;
+    } else {
+      temp[k] = arr[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i <= mid) {
+    temp[k] = arr[i];
+    i++;
+    k++;
+  }
+
+  while (j <= high) {
+    temp[k] = arr[j];
+    j++;
+    k++;
+  }
+
+  for (let i = low, j = 0; i <= high; i++ , j++) {
+    arr[i] = temp[j];
+  }
+}
+
+let arr = [];
+for (let i = 0; i < 10; i++) {
+  arr.push(Math.floor(Math.random() * 100));
+}
+arr = [9, 1, 5, 3, 4, 2, 6, 8, 7]
+console.time('sortTime');
+let sortedArr = mergeSort(arr);
+console.timeEnd('sortTime');
+console.log(`Old Array: ${arr}`);
+console.log(`Sorted Array: ${sortedArr}`);
