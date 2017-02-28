0434f3f9781c329f2a4d72b2ce5e2d95e12c26df
lon <dove2580@live.com>
2017-02-28 19:42:45 +0800

    improve

--- search/fibonacci-search.js
+++ search/fibonacci-search.js
@@ -3,7 +3,7 @@ let fibonacci = (maxValue) => {
   let i = 2;
   while (true) {
     fibonacciArr[i] = fibonacciArr[i - 1] + fibonacciArr[i - 2];
-    if (fibonacciArr[i] > maxValue) break;
+    if (fibonacciArr[i] - 1 > maxValue) break;
     i++;
   }
   return fibonacciArr;
@@ -18,11 +18,28 @@ let fibonacciSearch = (arr, key) => {
   }
   let low = 0;
   let high = arr.length - 1;
-  let mid;
 
-  let fibonacciArr = fibonacci(arr[high]);
-  console.log(fibonacciArr);
-  // TODO
+  let fibonacciArr = fibonacci(arr.length);
+  let k = fibonacciArr.length - 1;
+  let temp = arr.concat();
+  for (let i = arr.length; i < fibonacciArr[k] - 1; i++) {
+    temp[i] = arr[arr.length - 1];
+  }
+
+  while (low <= high) {
+    let mid = low + fibonacciArr[k - 1] - 1;
+    if (key === temp[mid]) {
+      if (mid < arr.length) return mid;
+      return arr.length - 1;
+    }
+    if (key > temp[mid]) {
+      low = mid + 1;
+      k -= 2;
+    } else {
+      high = mid - 1;
+      k -= 1;
+    }
+  }
 }
 
 let arr = [];
@@ -32,7 +49,7 @@ for (let i = 0; i < 20; i++) {
 arr.sort((item1, item2) => {
   return item1 > item2 ? 1 : -1;
 });
-let index = Math.floor(Math.random() * 19);
+let index = Math.floor(Math.random() * 20);
 let key = arr[index];
 console.time('Search Time');
 let findIndex = fibonacciSearch(arr, key);
